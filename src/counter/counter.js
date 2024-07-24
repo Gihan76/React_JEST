d4d6c201f1375579d9feee9f8a862d3ed84833ce
Gihan76 <gihantharaka76@gmail.com>
2024-07-24 19:33:17 +0530

    initial unit test case for counter

--- src/counter/counter.js
+++ src/counter/counter.js
@@ -1,11 +1,33 @@
 import { useState } from "react";
 
-export const Counter = () => {
-  const [count, setCount] = useState(0);
+export const Counter = ({initialCount}) => {
+  const [count, setCount] = useState(initialCount);
+
+  const IncreaseCount = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const DecreaseCount = () => {
+    setCount((prev) => prev !== 0 ? prev - 1 : 0);
+  };
+
+  const ResetCount = () => {
+    setCount(initialCount);
+  };
+
+  const SwitchSignsCount = () => {
+    setCount((prev) => prev * -1);
+  };
 
   return (
     <>
-      <h1>This is counter UI</h1>
+      <h1 data-testid="final_count">{count}</h1>
+      <div>
+        <button onClick={IncreaseCount}>Increase</button>
+        <button onClick={DecreaseCount}>Decrease</button>
+        <button onClick={ResetCount}>Reset</button>
+        <button onClick={SwitchSignsCount}>Switch Signs</button>
+      </div>
     </>
   );
 };
